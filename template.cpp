a86b7250336cd756b4465aaa2f07a2399ee863be
DeepakReddyVelagala <vksdeepakreddy@gmail.com>
2023-05-14 10:56:58 +0530

    adding initial template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+# include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+void solve(){
+
+}
+
+int main(){
+    int tt;
+    cin>>tt;
+    while(tt--){
+        solve();
+    }
+    return 0;
+}
